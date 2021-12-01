d4690de8c5b95a1905d5823f29e3960ca098aff7
RajPorus19 <raj.porus.hiruthayaraj@gmail.com>
2021-12-01 18:38:08 +0100

    live.py

--- live.py
+++ live.py
@@ -1,5 +1,6 @@
 import datetime
 import dash
+from dash import State
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly
@@ -18,9 +19,14 @@ app.layout = html.Div(
         html.H4('Opendata Torrent'),
         html.Div(id='live-update-text'),
         dcc.Graph(id='live-update-graph'),
+        dcc.Graph(id='live-update-map'),
+    html.Div(dcc.Input(id='input-on-submit', type='text')),
+    html.Button('Submit', id='submit-val', n_clicks=0),
+    html.Div(id='container-button-basic',
+             children='Enter a value and press submit'),
         dcc.Interval(
             id='interval-component',
-            interval=5*1000, # in milliseconds
+            interval=1*1000, # in milliseconds
             n_intervals=0
         )
     ])
@@ -44,7 +50,23 @@ def update_metrics(n):
 def update_graph_live(n):
     return get_bar_fig()
 
+@app.callback(Output('live-update-map', 'figure'),
+              Input('interval-component', 'n_intervals'))
+def update_map_live(n):
+    return get_geo_fig()
 
+# button
+
+@app.callback(
+    Output('container-button-basic', 'children'),
+    Input('submit-val', 'n_clicks'),
+    State('input-on-submit', 'value')
+)
+def update_output(n_clicks, value):
+    return 'The input value was "{}" and the button has been clicked {} times'.format(
+        value,
+        n_clicks
+    )
 
 
 if __name__ == '__main__':
