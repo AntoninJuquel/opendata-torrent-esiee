9b5f0a30d43355ca671c4af012fe871738316dff
RajPorus19 <raj.porus.hiruthayaraj@gmail.com>
2021-12-10 12:30:28 +0100

    pacman.conf

--- containers.py
+++ containers.py
@@ -1,13 +1,13 @@
 import docker
 from io import StringIO
 client = docker.from_env()
-f = open('./runs.tar', 'wb')
-container = client.containers.create("torrent", ["python","main.py"],network_mode="host")
-print("searching IPs")
+f = open('./bulkTorrents.tar', 'wb')
+container = client.containers.create("torrent", ["python","scrap.py"],network_mode="host")
+print("scrapping torrents")
 container.start()
-container.exec_run(["python","main.py"])
-print("finished scrapping IPs")
-strm,status = container.get_archive("/root/runs/")
+container.exec_run(["python","scrap.py"])
+print("finished scrapping torrents")
+strm,status = container.get_archive("/root/bulkTorrents/")
 for d in strm:
     f.write(d)
 f.close()
