f05735c191d062fdf015deb9d1627f81713601e5
RajPorus19 <raj.porus.hiruthayaraj@gmail.com>
2021-12-28 08:07:53 +0100

    fetching torrent files info

--- live.py
+++ live.py
@@ -6,11 +6,13 @@ import dash_html_components as html
 import plotly
 from app import get_bar_fig, get_geo_fig
 from dash.dependencies import Input, Output
+from progress import ProgressManager
+
+from scrap import execute_scrapping
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-#app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-app = dash.Dash(__name__)
 app.layout = html.Div(
     html.Div([
         html.H4('Opendata Torrent'),
@@ -19,9 +21,13 @@ app.layout = html.Div(
         html.Div(dcc.Input(id='num-of-torrent-input', type='text')),
         html.Button(children='scrape new torrents', id='scrape-new-torrents-btn', n_clicks=0),
         html.Div(id='live-update-text'),
+        html.Div(id='live-update-text-2'),
         dcc.Graph(id='live-update-graph'),
         dcc.Graph(id='live-update-map'),
-    html.Div(dcc.Input(id='input-on-submit', type='text')),
+
+        # useless text fields
+        html.Div(id='live-update-text-3'),
+        html.Div(dcc.Input(id='input-on-submit', type='text')),
     html.Div(id='container-button-basic',
              children='Enter a value and press submit'),
         dcc.Interval(
@@ -33,16 +39,6 @@ app.layout = html.Div(
     ])
 )
 
-i = 1
-
-@app.callback(Output('live-update-text', 'children'),
-              Input('interval-component', 'n_intervals'))
-def update_metrics(n):
-    global i
-    i+=1
-    return [
-            i,
-    ]
 
 @app.callback(Output('live-update-graph', 'figure'),
               Input('interval-component', 'n_intervals'))
@@ -69,13 +65,37 @@ def pause_play(n_clicks):
 
 
 @app.callback(
+    Output('live-update-text-2', 'children'),
     Input('scrape-new-torrents-btn', 'n_clicks'),
-    State('num-of-torrent-input', 'value')
+    State('num-of-torrent-input', 'value'),
 )
-def scrape_torrent_files(value):
+def scrape_torrent_files(n_clicks, value):
+    if value is None:
+        return ""
     if value.isnumeric():
         value = int(value)
         execute_scrapping(value)
+        return "Scrapped {} files !".format(value * 8)
+    return ""
+
+
+@app.callback(
+    Output('live-update-text-3', 'children'),
+    Input('num-of-torrent-input', 'value')
+)
+def print_num_of_torrents(value):
+    if value is None:
+        return ""
+    if value.isnumeric():
+        ProgressManager().write_line("This will fetch {} torrent files.".format(int(value) * 8))
+        return ""
+
+
+@app.callback(Output('live-update-text', 'children'),
+              Input('interval-component', 'n_intervals'))
+def show_progress(n):
+    text = ProgressManager().show()
+    return text
 
 
 if __name__ == '__main__':
